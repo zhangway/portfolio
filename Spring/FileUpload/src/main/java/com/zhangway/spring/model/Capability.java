db619ad2d7ae9e3f8f2a4f048c43ab7c9d872a84
Wei Zhang <djang.wei@gmail.com>
2013-11-04 16:02:33 +0100

    add capability class

---
+++ Spring/FileUpload/src/main/java/com/zhangway/spring/model/Capability.java
@@ -0,0 +1,12 @@
+package com.zhangway.spring.model;
+
+import java.util.ArrayList;
+
+public class Capability {
+	
+	private String nonce;
+	private String resourceId;
+	private ArrayList Caveats; 
+	private String signature;
+
+}
---
+++ Spring/FileUpload/src/main/java/com/zhangway/spring/model/Caveat.java
@@ -0,0 +1,8 @@
+package com.zhangway.spring.model;
+
+public class Caveat {
+	
+	private String predicate;
+	private String function;
+
+}
