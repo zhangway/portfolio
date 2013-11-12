634a2b3715fbc100f8316ff7ce3d049f0a84ea1c
Wei Zhang <djang.wei@gmail.com>
2013-11-12 16:51:41 +0100

    add runkeeper.properties

--- Spring/runkeeperClient/src/main/java/no/uit/zhangway/RunkeeperController.java
+++ Spring/runkeeperClient/src/main/java/no/uit/zhangway/RunkeeperController.java
@@ -5,13 +5,21 @@ import org.springframework.stereotype.Controller;
 
 
 @Controller
-public class RunkeeperController {
+public class RunkeeperController {	
+
+	private RunkeeperService runkeeperService;
 	
+	/*
+	@RequestMapping("/connectrunkeeper")
+	public String photos(Model model) throws Exception {
+		model.addAttribute("photoIds", sparklrService.getSparklrPhotoIds());
+		model.addAttribute("path", "photos");
+		return "sparklr";
+	}
+	*/
 	public void setRunkeeperService(RunkeeperService runkeeperService) {
 		this.runkeeperService = runkeeperService;
 	}
-
-	private RunkeeperService runkeeperService;
 	
 
 }
--- Spring/runkeeperClient/src/main/java/no/uit/zhangway/RunkeeperServiceImpl.java
+++ Spring/runkeeperClient/src/main/java/no/uit/zhangway/RunkeeperServiceImpl.java
@@ -4,10 +4,29 @@ import org.springframework.web.client.RestOperations;
 
 public class RunkeeperServiceImpl implements RunkeeperService {
 	
-	private String sparklrPhotoListURL;
 	private String sparklrTrustedMessageURL;
 	private String sparklrPhotoURLPattern;
 	private RestOperations sparklrRestTemplate;
 	private RestOperations trustedClientRestTemplate;
+	
+	private String sparklrPhotoListURL;
+	
+	public void setSparklrPhotoListURL(String sparklrPhotoListURL) {
+		this.sparklrPhotoListURL = sparklrPhotoListURL;
+	}
+	public void setSparklrTrustedMessageURL(String sparklrTrustedMessageURL) {
+		this.sparklrTrustedMessageURL = sparklrTrustedMessageURL;
+	}
+	public void setSparklrPhotoURLPattern(String sparklrPhotoURLPattern) {
+		this.sparklrPhotoURLPattern = sparklrPhotoURLPattern;
+	}
+	public void setSparklrRestTemplate(RestOperations sparklrRestTemplate) {
+		this.sparklrRestTemplate = sparklrRestTemplate;
+	}
+	public void setTrustedClientRestTemplate(
+			RestOperations trustedClientRestTemplate) {
+		this.trustedClientRestTemplate = trustedClientRestTemplate;
+	}
+
 
 }
